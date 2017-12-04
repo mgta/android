c9b4e50f3f84d1676b90d9641cff4c6b15379b43
tobiaskaminsky <tobias@kaminsky.me>
2017-12-04 10:50:38 +0100

    - replace Toast with Snackbar - remove not used code

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -1445,9 +1445,6 @@ public class FileDisplayActivity extends HookActivity
                             case OCFileListFragment.DOWNLOAD_SEND:
                                 sendDownloadedFile();
                                 break;
-                            case OCFileListFragment.DOWNLOAD_SET_AS:
-                                setPictureAs();
-                                break;
                             default:
                                 // do nothing
                                 break;
@@ -1605,7 +1602,7 @@ public class FileDisplayActivity extends HookActivity
                 mUploaderBinder = null;
             }
         }
-    };    
+    }
 
     private MediaServiceConnection newMediaConnection(){
         return new MediaServiceConnection();
@@ -1635,7 +1632,7 @@ public class FileDisplayActivity extends HookActivity
                 mMediaServiceBinder = null;
             }
         }
-    };
+    }
 
     /**
      * Updates the view associated to the activity after the finish of some operation over files
@@ -1981,12 +1978,6 @@ public class FileDisplayActivity extends HookActivity
         getFileOperationsHelper().sendDownloadedFile(mWaitingToSend);
         mWaitingToSend = null;
     }
-
-    private void setPictureAs() {
-        getFileOperationsHelper().setPictureAs(mWaitingToSend);
-        mWaitingToSend = null;
-    }
-
 
     /**
      * Requests the download of the received {@link OCFile} , updates the UI
